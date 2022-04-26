2911c3e99e3b700221c59ed3c6dfccf043926a11
Sean Leverenz <snlevere@mtu.edu>
2022-04-25 22:43:58 -0400

    changes

--- inst_courses.php
+++ inst_courses.php
@@ -1,3 +1,7 @@
+<?php
+    session_start();
+    require "db.php";
+?>
 <html>
     <body>
         <?php include 'inst_sidebar.html'; ?>
@@ -7,6 +11,47 @@
                 margin-left: 20%;
             }
         </style>
-        
+        <h1>Courses</h1>
+        <div class="currentCourses">
+            <table>
+                <tbody>
+                    <?php
+                        /* Fetch the current user's course */
+                        $currentUser = $_SESSION["username"];
+                        try {
+                            $dbh = connectDB();
+                            $statement = $dbh->query("select course.course_id, course.title from course left outer join teaches on course.course_id = teahces.course_id where inst_name = '$currentUser'");
+                            $statement->execute();
+                            $dbh = null;
+                        } catch(PDOException $e) {
+                            print "Error!" . $e->getMessage() . "<br/>";
+                            die();
+                        } 
+                        while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
+                            $courseID = $row['course_id'];
+                            $courseTitle = $row['title'];
+                            ?> <h3><?php echo $courseID ?> <?php echo $courseTitle ?></h3> <?php
+                            printStudents($courseID);
+                        }
+                        
+                        function printStudents ($courseID) {
+                            try {
+                                $dbh = connectDB();
+                                $statement = $dbh->query("select stu_name from takes where course_id = '$courseID'");
+                                $statement->execute();
+                                $dbh = null;
+                            } catch(PDOException $e) {
+                                print "Error!" . $e->getMessage() . "<br/>";
+                                die();
+                            } 
+                            while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
+                                $student = $row['student'];
+                                ?> <?php echo $student ?> <br> <?php
+                            }
+                        }
+                    ?>
+                </tbody>
+            </table>
+        </div>
     </body>
 </html>
